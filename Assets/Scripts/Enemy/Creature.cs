f8f42f02babbaf59fc54b62a85558a6085bfeb69
Rakichel <pcy5006@gmail.com>
2023-03-24 12:02:59 +0900

    add : Enemy in Creature FoodSoldier
    크리쳐 스크립트와 보병스크립트

---
+++ Assets/Scripts/Enemy/Creature.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature
+{
+    public enum EnemyType
+    {
+        Humanoid,
+        Beast,
+        Demon
+    }
+
+    public enum AttackType
+    {
+        Physical,
+        Magical
+    }
+
+    public enum State
+    {
+        Idle,
+        Attacking,
+        Defending,
+        Dead
+    }
+
+    public EnemyType Type { get; set; }
+    public AttackType Attack { get; set; }
+    public State CurrentState { get; set; }
+
+    public void Attack()
+    {
+        if (CurrentState == State.Attacking)
+        {
+            Debug.Log("Attacking with " + Attack.ToString() + " attack!");
+        }
+    }
+
+    public void Defend()
+    {
+        if (CurrentState == State.Defending)
+        {
+            Debug.Log("Defending against enemy " + Type.ToString() + "!");
+        }
+    }
+
+    public void Die()
+    {
+        if (CurrentState != State.Dead)
+        {
+            CurrentState = State.Dead;
+            Debug.Log("Creature has died.");
+        }
+    }
+}
+
---
+++ Assets/Scripts/Enemy/FootSoldier.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSoldier : Creature
+{
+        public FootSoldier()
+        {
+            //Type = EnemyType.Enemy; // player CreatureType
+            //Attack = AttackType.Physical;
+            //CurrentState = State.Idle;
+        }
+
+        public void kamikaze()
+        {
+            //if (CurrentState == State.Idle) // walk jump attack hit die
+            //{
+            //    Debug.Log("Orc goes into a berserk rage!");
+            //    Attack();
+            //}
+        }
+    }
+}
