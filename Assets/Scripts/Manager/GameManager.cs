6258303444ae46ccc8d555fe6242a7e5215f5700
h0000n <anjunghoon2716@gmail.com>
2023-04-10 17:29:11 +0900

    게임매니저, 카메라, 빌드, 피직스 세팅
    타임 스케일 추가, 카메라 효과 추가, 피직스 세팅 수정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,21 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Manager
 {
+    
     public class GameManager : Singleton<GameManager>
     {
+        public float TimeS = 0.2f;
         // Start is called before the first frame update
         private void Start()
         {
-
+            Time.timeScale = TimeS;
         }
 
+        private void FixedUpdate()
+        {
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+            Physics2D.Simulate(0.02f);
+        }
         // Update is called once per frame
         private void Update()
         {
-
+            if(Input.GetKey(KeyCode.Minus))
+            {
+                SceneManager.LoadScene(gameObject.scene.name);
+            }
         }
     }
 
