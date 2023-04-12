2b680e630807dfe0d6ab34bb9e953fbd518f97bd
h0000n <anjunghoon2716@gmail.com>
2023-04-12 11:21:39 +0900

    매니저 수정
    로직 수정

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -8,24 +8,30 @@ namespace Manager
     
     public class GameManager : Singleton<GameManager>
     {
-        public float TimeS = 0.2f;
+        public float TimeS = 1f;
         // Start is called before the first frame update
         private void Start()
         {
-            Time.timeScale = TimeS;
+            
         }
 
         private void FixedUpdate()
         {
+            Time.timeScale = TimeS;
             Time.fixedDeltaTime = 0.02f * Time.timeScale;
             Physics2D.Simulate(0.02f);
         }
         // Update is called once per frame
         private void Update()
         {
-            if(Input.GetKey(KeyCode.Minus))
+            if (Input.GetKeyDown(KeyCode.F))
             {
-                SceneManager.LoadScene(gameObject.scene.name);
+                if (TimeS == 0.2f)
+                    TimeS = 1;
+                else
+                {
+                    TimeS = 0.2f;
+                }
             }
         }
     }
