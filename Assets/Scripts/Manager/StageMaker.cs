d122c026eb2e2d07f66e1af04eb8ad41824cf38d
h0000n <anjunghoon2716@gmail.com>
2023-05-01 23:14:31 +0900

    add : EnemyManger, fix : StageMaker

--- Assets/Scripts/Manager/StageMaker.cs
+++ Assets/Scripts/Manager/StageMaker.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class StageMaker : MonoBehaviour
 {
     [SerializeField] private int BlockCount;
-    [SerializeField] private GameObject[] Block;
     [SerializeField] private Transform StartPos;
     [SerializeField] private Transform LastPos;
     [SerializeField] private float DistanceMin;
     [SerializeField] private float DistanceMax;
+    public float[] BlockFloor;
+    public GameObject[] Block;
     public GameObject[] CurrentBlock;
     private Transform Player;
+    public UnityEvent StageMakeEvent;
     // Start is called before the first frame update
     void Start()
     {
         CurrentBlock = new GameObject[BlockCount];
+        BlockFloor = new float[BlockCount];
         LastPos = StartPos;
         StageSpawn();
         Player = GameObject.Find("StageTest").transform;
@@ -30,13 +34,29 @@ public class StageMaker : MonoBehaviour
         }
     }
 
-    void StageSpawn()
+    public void StageSpawn()
     {
         for (int i = 0; i < BlockCount; i++)
         {
             var newBlock = Instantiate(Block[Random.Range(0, Block.Length)], LastPos.position + new Vector3(Random.Range(DistanceMin, DistanceMax + 1), 0, 0), gameObject.transform.rotation);
             LastPos = newBlock.transform;
             CurrentBlock[i] = newBlock;
+            switch(newBlock.name)
+            {
+                case "Floor1":
+                    BlockFloor[i] = 1;
+                    break;
+                case "Floor2":
+                    BlockFloor[i] = 2;
+                    break;
+                case "Florr3":
+                    BlockFloor[i] = 3;
+                    break;
+                default:
+                    BlockFloor[i] = 1;
+                    break;
+            }
         }
+        StageMakeEvent.Invoke();
     }
 }
