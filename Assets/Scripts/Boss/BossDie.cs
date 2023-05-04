bb6efe3bfb54ad20751749c9836da8929c38cc71
DongSun <duaehdtjs20@gmail.com>
2023-05-04 21:17:20 +0900

    add: samurai commander

--- Assets/Scripts/Boss/BossDie.cs
+++ Assets/Scripts/Boss/BossDie.cs
@@ -1,7 +1,15 @@
+using UnityEngine;
+
 public class BossDie : IBossTodo
 {
+    private float timer = 0f;
+    public BossDie() { }
     public void Work()
     {
-
+        timer += Time.unscaledDeltaTime;
+        if (timer > 1f)
+        {
+            // 씬로드 함수 호출
+        }
     }
 }
