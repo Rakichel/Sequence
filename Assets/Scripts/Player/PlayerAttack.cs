504c9dfe7b751d383a205ea65737f104e3f2c270
DongSun <duaehdtjs20@gmail.com>
2023-05-04 11:00:30 +0900

    fix: player attack area

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -215,7 +215,7 @@ namespace PlayerInfo
             // 보는 방향 기준으로 공격 방향 지정
             if (_player.State == PlayerState.Counter)
             {
-                return Physics2D.OverlapBoxAll(transform.position + new Vector3(0f, 0.5f), new Vector3(2f, 1f), 0f, 1 << 8 | 1 << 9);
+                return Physics2D.OverlapBoxAll(transform.position + new Vector3(0f, 0.75f), new Vector3(3.5f, 1.5f), 0f, 1 << 8 | 1 << 9);
             }
             else if (_player.State == PlayerState.Landed)
             {
@@ -223,11 +223,11 @@ namespace PlayerInfo
             }
             else if (_player.Direction == PlayerDirection.Right)
             {
-                return Physics2D.OverlapBoxAll(transform.position + new Vector3(0.75f, 0.75f), new Vector3(1f, 1f), 0f, 1 << 8 | 1 << 9);
+                return Physics2D.OverlapBoxAll(transform.position + new Vector3(1f, 1f), new Vector3(1.5f, 2f), 0f, 1 << 8 | 1 << 9);
             }
             else
             {
-                return Physics2D.OverlapBoxAll(transform.position + new Vector3(-0.75f, 0.75f), new Vector3(1f, 1f), 0f, 1 << 8 | 1 << 9);
+                return Physics2D.OverlapBoxAll(transform.position + new Vector3(-1f, 1f), new Vector3(1.5f, 2f), 0f, 1 << 8 | 1 << 9);
             }
         }
 
@@ -238,14 +238,14 @@ namespace PlayerInfo
                 Gizmos.color = new Color(1f, 0f, 0f, 0.5f);
                 if (_player.Direction == PlayerDirection.Right)
                 {
-                    Gizmos.DrawCube(transform.position + new Vector3(0.75f, 0.5f), new Vector3(1f, 1f));
+                    Gizmos.DrawCube(transform.position + new Vector3(1f, 1f), new Vector3(1.5f, 2f));
                 }
                 else
                 {
-                    Gizmos.DrawCube(transform.position + new Vector3(-0.75f, 0.5f), new Vector3(1f, 1f));
+                    Gizmos.DrawCube(transform.position + new Vector3(-1f, 1f), new Vector3(1.5f, 2f));
                 }
                 Gizmos.color = new Color(0f, 0f, 1f, 0.5f);
-                Gizmos.DrawCube(transform.position + new Vector3(0f, 0.5f), new Vector3(2f, 1f));
+                Gizmos.DrawCube(transform.position + new Vector3(0f, 0.75f), new Vector3(3.5f, 1.5f));
             }
         }
     }
