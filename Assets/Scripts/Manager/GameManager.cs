15ac77e1796dc5c4fdbf3f611ce4808b211d6c59
h0000n <anjunghoon2716@gmail.com>
2023-05-07 18:24:28 +0900

    Add : SkillUi, GameTimer
    스킬창 추가, 게임타이머 추가

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -8,6 +8,7 @@ namespace Manager
     
     public class GameManager : Singleton<GameManager>
     {
+        public static float GameTimer;
         public float TimeS = 1f;
         public int KillCount = 0;
         public int EnemyLevel = 1;
@@ -17,15 +18,23 @@ namespace Manager
         [SerializeField] private float _fade = 0.1f;
         private bool dieShader = true;
 
+        private void Awake()
+        {
+            if(SceneManager.GetActiveScene().name == "Japanese landscape")
+            {
+                GameTimer = 0f;
+            }
+        }
         private void FixedUpdate()
         {
             Physics2D.Simulate(0.02f);
+            GameTimer += Time.unscaledDeltaTime;
+
         }
         private void Update()
         {
             Time.timeScale = TimeS;
             Time.fixedDeltaTime = 0.02f * Time.timeScale;
-            
 
             if (Input.GetKeyDown(KeyCode.T))
             {
